3f6bf324d85fb4f5e2aa89b5e85274ffa01da535
Paul Rohozhyn <dev.rohozyn@gmail.com>
2024-10-23 21:47:55 +0200

    init

---
+++ task1.py
@@ -0,0 +1,12 @@
+#task1.py
+
+def test():
+    pass
+
+# Main
+if __name__ == '__main__':
+    try:
+        test()
+    except (ValueError) as e:
+        print(f'Something went wrong, {e}')
+        sys.exit(0)
