653d0b252b2a7be05649ad8786d25929b7acbf16
Paul Rohozhyn <dev.rohozyn@gmail.com>
2024-10-28 09:13:21 +0100

    add hm

--- task1.py
+++ task1.py
@@ -1,12 +1,30 @@
-#task1.py
+from pulp import LpMaximize, LpProblem, LpVariable
 
-def test():
-    pass
+# Model
+model = LpProblem(name="maximize-drinks-production", sense=LpMaximize)
+# Lemonade and Fruit Juice
+lemonade = LpVariable(name="lemonade", lowBound=0, cat='Continuous')
+fruit_juice = LpVariable(name="fruit_juice", lowBound=0, cat='Continuous')
+# Maximize the total amount of lemonade and fruit juice produced
+model += lemonade + fruit_juice, "Total_Production"
+# Add the constraints based on the available resources:
+# Water constraint: 2*lemonade + 1*fruit_juice <= 100
+model += 2 * lemonade + 1 * fruit_juice <= 100, "Water_Constraint"
+# Sugar constraint: 1*lemonade <= 50
+model += lemonade <= 50, "Sugar_Constraint"
+# Lemon juice constraint: 1*lemonade <= 30
+model += lemonade <= 30, "Lemon_Juice_Constraint"
+# Fruit puree constraint: 2*fruit_juice <= 40
+model += 2 * fruit_juice <= 40, "Fruit_Puree_Constraint"
+# Solve the optimization problem
+model.solve()
 
-# Main
-if __name__ == '__main__':
-    try:
-        test()
-    except (ValueError) as e:
-        print(f'Something went wrong, {e}')
-        sys.exit(0)
+# Results
+print("Result")
+print("----------------------------------------------")
+print(f"Lemonade produced: {lemonade.varValue}")
+print("----------------------------------------------")
+print(f"Fruit juice produced: {fruit_juice.varValue}")
+print("----------------------------------------------")
+print(f"Total production: {lemonade.varValue + fruit_juice.varValue}")
+print("----------------------------------------------")
--- task2.py
+++ task2.py
@@ -1,12 +1,56 @@
-# task2.py
+import numpy as np
+import scipy.integrate as spi
+import matplotlib.pyplot as plt
 
-def test():
-    pass
+
+# function f(x) = sin(x)
+def f(x):
+    return np.sin(x)
+
+
+# monte carlo
+def monte_carlo(a, b):
+    N = 1000  # count of random points
+    x_random = np.random.uniform(a, b, N)
+    y_random = np.random.uniform(0, 1, N)  # sin(x) ≤ 1 for all "x" on intergal [0, π]
+    # gray part sin(x)
+    under_curve = y_random < f(x_random)
+    integral_mc = (b - a) * 1 * np.mean(under_curve)
+    # quad
+    result_quad, error_quad = spi.quad(f, a, b)
+    # show result
+    print('---------------------------------------------------------')
+    print(f"Integral by Monte-Carlo: {integral_mc}")
+    print('---------------------------------------------------------')
+    print(f"Integral by quad: {result_quad}, error: {error_quad}")
+    print('---------------------------------------------------------')
+
 
 # Main
 if __name__ == '__main__':
     try:
-        test()
+        # limits of integration
+        a = 0 
+        b = np.pi
+        # monte carlo
+        monte_carlo(a, b)
+        #visualisation
+        x = np.linspace(0, np.pi, 400)
+        y = f(x)
+        fig, ax = plt.subplots()
+        ax.plot(x, y, 'r', linewidth=2)
+        ix = np.linspace(a, b)
+        iy = f(ix)
+        ax.fill_between(ix, iy, color='gray', alpha=0.3)
+        ax.set_xlim([0, np.pi])
+        ax.set_ylim([0, 1.1])
+        ax.set_xlabel('x')
+        ax.set_ylabel('f(x)')
+        ax.axvline(x=a, color='gray', linestyle='--')
+        ax.axvline(x=b, color='gray', linestyle='--')
+        ax.set_title('Integral f(x) = sin(x) from 0 to π')
+        plt.grid(True)
+        plt.show()
     except (ValueError) as e:
         print(f'Something went wrong, {e}')
         sys.exit(0)
